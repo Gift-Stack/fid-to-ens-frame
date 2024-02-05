b643c19613e838673e153aac0f2aec74a93cdb8a
GiFTED <gkenny896@gmail.com>
2024-02-05 13:59:47 +0100

    fix: add new frames metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,25 @@
+import { getFrameMetadata } from "@coinbase/onchainkit";
 import HomePage from "./home";
+import { Metadata } from "next";
+
+const postUrl = process.env["HOST"] + `/api/ens`;
+const frameImageUrl = process.env["HOST"] + `/ens.webp`;
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: "Click to get your ENS from your FID",
+    },
+  ],
+  image: frameImageUrl,
+  post_url: postUrl,
+});
+
+export const metadata: Metadata = {
+  manifest: "/manifest.json",
+  other: {
+    ...frameMetadata,
+  },
+};
 
 export default async function Home() {
   return <HomePage />;
